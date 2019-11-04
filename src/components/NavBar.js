f1873d3b123f0f3cc99c345729d22c179ef7dbfe
Olabanji121 <olabanjiabimbola@gmail.com>
2019-11-04 11:31:08 +0100

    updates

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,18 +4,26 @@ import { Link } from "react-router-dom";
 const NavBar = () => {
     return (
 			<nav className="navbar navbar-expand-sm navrbar-light bg-light">
-				<Link to="/" className="navbar-brand pl-3 text-orange text-slanted">
-					<h4>Trivia Quiz</h4>
-				</Link>
+				<div className="pl-3">
+					<Link
+						to="/"
+						className="navbar-brand  text-orange text-slanted border border-danger rounded"
+					>
+						<h4 style={{ paddingLeft: "10px", paddingRight: "10px" }}>
+							Trivia Quiz
+						</h4>
+					</Link>
+				</div>
+
 				<div className="collapse navbar-collapse show ml-sm-5 justify-content-end">
 					<ul className="navbar-nav">
-						<li className="navbar-item pl-3">
-							<Link to="/" className="nav-link text-secondary">
+						<li className="navbar-item  pl-3">
+							<Link to="/" className="nav-link  text-info">
 								<h5>Home</h5>
 							</Link>
 						</li>
-						<li className="navbar-item pl-3">
-							<Link to="/game" className="nav-link text-secondary">
+						<li className="navbar-item  pl-3">
+							<Link to="/game" className="nav-link text-info">
 								<h5>Quiz</h5>
 							</Link>
 						</li>
